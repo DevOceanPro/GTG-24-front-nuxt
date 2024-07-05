1b7c4d9a17c963a9d2df93840d8a066ee3b5b34a
Maksym_Kurbatov <Maksym_Kurbatov@epam.com>
2024-07-05 12:32:11 +0300

    add vuetify and pinia modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,24 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
-import {defineNuxtConfig} from "nuxt/config";
+import { defineNuxtConfig } from 'nuxt/config'
+import vuetify from 'vite-plugin-vuetify'
 
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-
+  devtools: { enabled: true },
+  modules: [
+    '@pinia/nuxt'
+  ],
+  css: [
+    'vuetify/styles',
+    '@mdi/font/css/materialdesignicons.min.css',
+  ],
+  build: {
+    transpile: ['vuetify'],
+  },
+  vite: {
+    ssr: {
+      noExternal: ['vuetify'],
+    },
+    plugins: [
+      vuetify()
+    ],
+  },
 })
